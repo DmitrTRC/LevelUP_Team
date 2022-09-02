1900184f77b2220820ceb005e9f3cc57bceb3707
Dmitry Morozov <9984398@gmail.com>
2022-09-03 01:38:39 +0300

    Json parser : db_file parsed

--- main.cpp
+++ main.cpp
@@ -1,7 +1,30 @@
+#include <nlohmann/json.hpp>
+
+
 #include <iostream>
+#include <fstream>
 
 
 int main () {
-    std::cout << "Hello, World!" << std::endl;
+
+    using json = nlohmann::json;
+    const std::string db_filename = "team.json";
+
+    std::ifstream team_db (db_filename);
+    json data = json::parse (team_db);
+
+    auto team = data["team"];
+
+    for (auto &member: team) {
+        std::cout << "ID :" << member["id"] << std::endl;
+        std::cout << "Name : " << member["name"] << std::endl;
+        std::cout << "Lastname :" << member["lastName"] << std::endl;
+        std::cout << "Email :" << member["email"] << std::endl;
+        std::cout << "Github link : :" << member["github"] << std::endl;
+        std::cout << std::endl;
+
+    }
+
+
     return 0;
 }
